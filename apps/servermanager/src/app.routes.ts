254fa560d34fcc22af5c27b4dec99e5ee4eb0e93
jLabaj <labaj.jakub@gmail.com>
2024-11-01 00:16:41 +0100

    adjustments

--- apps/servermanager/src/app.routes.ts
+++ apps/servermanager/src/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from "@angular/router";
 import { OverviewComponent } from "./app/components/overview/overview.component";
 import { DashboardComponent } from "./app/components/dashboard/dashboard.component";
-import { ServiceManagerComponent } from "./app/server-manager.component";
 
 export const routes: Routes = [
-    { path: '', component: ServiceManagerComponent },
+    { path: '', redirectTo: 'overview', pathMatch: 'full' },
     { path: 'overview', component: OverviewComponent },
     { path: 'dashboard', component: DashboardComponent },
   ];
