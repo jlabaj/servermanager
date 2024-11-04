4d8479d7479c19bd53078922162dc84814461959
jlabaj <labaj.jakub@gmail.com>
2024-11-04 03:27:32 +0100

    api key remove

--- apps/servermanager/src/main.ts
+++ apps/servermanager/src/main.ts
@@ -11,7 +11,7 @@ import { routes } from './app.routes';
 import { ServiceManagerComponent } from './app/server-manager.component';
 
 bootstrapApplication(ServiceManagerComponent, {
-	providers: [importProvidersFrom(BrowserAnimationsModule), provideHttpClient(), provideRouter(routes), provideFirebaseApp(() => initializeApp({ "projectId": "servermanager-edac4", "appId": "1:1041712167433:web:8726db3b5e3e82ac1756d8", "databaseURL": "https://servermanager-edac4-default-rtdb.europe-west1.firebasedatabase.app", "storageBucket": "servermanager-edac4.appspot.com", "apiKey": "AIzaSyBCSNW9K6QChCKPVty3NiAsLicWoW3GuvE", "authDomain": "servermanager-edac4.firebaseapp.com", "messagingSenderId": "1041712167433" })),
+	providers: [importProvidersFrom(BrowserAnimationsModule), provideHttpClient(), provideRouter(routes), provideFirebaseApp(() => initializeApp({ "projectId": "servermanager-edac4", "appId": "1:1041712167433:web:8726db3b5e3e82ac1756d8", "databaseURL": "https://servermanager-edac4-default-rtdb.europe-west1.firebasedatabase.app", "storageBucket": "servermanager-edac4.appspot.com", "apiKey": "", "authDomain": "servermanager-edac4.firebaseapp.com", "messagingSenderId": "1041712167433" })),
 	provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()),
 	{
 		provide: APP_INITIALIZER,
