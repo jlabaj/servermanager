8678b9c7dccec294351f5d4e9ca30932a41cd76c
jlabaj <labaj.jakub@gmail.com>
2024-11-04 03:28:25 +0100

    remove api keys

--- apps/servermanager/src/environments/environment.prod.ts
+++ apps/servermanager/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
     appId: '1:718325986483:web:36c6d4743afa10f89b2cc6',
     databaseURL: 'https://bookstore-d261a-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'bookstore-d261a.appspot.com',
-    apiKey: 'AIzaSyBxnjDa-koJueXNnMAPuBYPIbCpzKgBoxw',
+    apiKey: '',
     authDomain: 'bookstore-d261a.firebaseapp.com',
     messagingSenderId: '718325986483',
   },
